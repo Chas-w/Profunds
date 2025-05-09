1f2cb337b0684aed108bf37350350bc4330dc2d3
Chase Dodge <75660257+Chas-w@users.noreply.github.com>
2025-05-09 16:40:45 -0400

    Flower Game

--- Assets/SceneAssets/Act 1/FlowerGrowing/FlowerGrowth.cs
+++ Assets/SceneAssets/Act 1/FlowerGrowing/FlowerGrowth.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class FlowerGrowth : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Animator flowerAnim;
+    Animator flowerAnim;
     public GameObject wateringCan;
 
     bool canWater; 
     void Start()
     {
+      flowerAnim = GetComponent<Animator>();
       Cursor.visible = false;
       Cursor.lockState = CursorLockMode.Confined;
 
@@ -17,10 +18,11 @@ public class FlowerGrowth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(flowerAnim.name);
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         wateringCan.transform.position = mousePosition;
 
-        if (Input.GetMouseButton(0) && canWater)
+        if (/*Input.GetMouseButton(0) && */ canWater && flowerAnim != null)
         {
             // flowerAnim.speed = 1f;
             flowerAnim.speed = 1f;
